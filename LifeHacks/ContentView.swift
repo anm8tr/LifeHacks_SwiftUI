7562917fbf46edff1aeaf9952746eed6866a433d
Richard Garrison <anm8tr@users.noreply.github.com>
2022-05-25 18:23:19 -0700

    Initial Commit

---
+++ LifeHacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LifeHacks
+//
+//  Created by Richard Garrison on 5/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LifeHacks/LifeHacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeHacksApp.swift
+//  LifeHacks
+//
+//  Created by Richard Garrison on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct LifeHacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
